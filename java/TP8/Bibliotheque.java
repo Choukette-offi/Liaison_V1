67ab604b9b023a7f4433236f7575f33312b133c6
Choukette-offi <clementmoisan16@gmail.com>
2025-04-04 09:29:18 +0200

    abdoul

--- java/TP8/Bibliotheque.java
+++ java/TP8/Bibliotheque.java
@@ -20,4 +20,46 @@ public class Bibliotheque {
         }
         return n_ls;
     }
+
+    public static boolean enCommun(List<Integer> liste1, List<Integer> liste2){
+        for(int i: liste1){
+            for(int j: liste2){
+                if(i == j){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static List<Integer> intersection(List<Integer> liste1, List<Integer> liste2){
+        Collections.sort(liste1);
+        Collections.sort(liste2);
+        int i =0, j=0;
+        List<Integer> res;
+        while (i<liste1.size() && j<liste2.size()) {
+            if(liste1.get(i) == liste2.get(j)){
+                res.add(liste1.get(i));
+            }
+            else if(liste1.get(i) > liste2.get(j)){
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        return res;
+    }
+
+    public static boolean indiceEgaleElement(List<Integer> liste1){
+        int i=0;
+        for(int val: liste1){
+            if(i == val){
+                return true;
+            }
+            i++;
+        }
+        return false;
+    }
+
 }

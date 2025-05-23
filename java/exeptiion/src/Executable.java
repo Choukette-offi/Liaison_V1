2b0b70e29d116aa7e3ee505bfd6fb5820dd17041
Choukette-offi <clementmoisan16@gmail.com>
2025-05-23 08:56:19 +0200

    sfghdhfgh

--- java/exeptiion/src/Executable.java
+++ java/exeptiion/src/Executable.java
@@ -1,13 +1,18 @@
 public class Executable {
-    Simple<String> s = new Simple<>("toto");
-    Simple<Integer> i = new Simple<>(12);
-    Simple<Double> d = new Simple<>(5.5);
-    Simple<String> s2 = new Simple<>();
-    Simple<Integer> i2 = new Simple<>();
-    Simple<Double> d2 = new Simple<>();
-    CoupleGenerique<Integer> n1 = new CoupleGenerique<Integer>(6, 2);
-    CoupleGenerique<Integer> n2 = new CoupleGenerique<Integer>(64, 21);
-    CoupleGenerique<Integer> n3 = new CoupleGenerique<Integer>(64, 3);
-    CoupleGenerique<Integer> n4 = new CoupleGenerique<Integer>(6, 2);
-    n1.compareTo(n2);
+    public static void main(String[] args) {
+        Simple<String> s = new Simple<>("toto");
+        Simple<Integer> i = new Simple<>(12);
+        Simple<Double> d = new Simple<>(5.5);
+        Simple<String> s2 = new Simple<>();
+        Simple<Integer> i2 = new Simple<>();
+        Simple<Double> d2 = new Simple<>();
+        CoupleGenerique<Integer> n1 = new CoupleGenerique<Integer>(6, 2);
+        CoupleGenerique<Integer> n2 = new CoupleGenerique<Integer>(64, 21);
+        CoupleGenerique<Integer> n3 = new CoupleGenerique<Integer>(64, 3);
+        CoupleGenerique<Integer> n4 = new CoupleGenerique<Integer>(6, 2);
+        n1.compareTo(n2);  
+        n3.compareTo(n2);
+        n1.compareTo(n4); 
+    }
+    
 }
